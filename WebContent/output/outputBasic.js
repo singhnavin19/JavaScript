a3da0b34b1525c37bfc06d6e5c5b889e0bdcfe33
Navin Singh <navinsingh19@gmail.com>
2020-04-19 17:06:24 +0530

    where to

---
+++ WebContent/output/outputBasic.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+
+// alert("hello");
+document.write("hello I am from document.write")
+
+// inner html 
+document.getElementById("innerTest").innerHTML='<input type="text" name="js" id="js">';
+
+//documen
