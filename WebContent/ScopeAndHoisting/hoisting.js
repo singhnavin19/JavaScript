268bc6e31fc6559b5b7f6e868f0deb636912108e
Navin Singh <navinsingh19@gmail.com>
2020-04-24 10:56:18 +0530

    pluralsight examples

---
+++ WebContent/ScopeAndHoisting/hoisting.js
@@ -0,0 +1,21 @@
+'use strict'
+console.log("------------Hoisting start here ");
+console.log(x);
+var x;
+
+y=20;
+console.log(y);
+var y;
+hello();
+
+function hello(){
+    console.log("from function :-hello");
+}
+
+
+// strictFp
+
+name="navin"
+console.log(" without any varible name=navin :- "+name)
+
+
--- WebContent/output/outputBasic.js
+++ WebContent/output/outputBasic.js
@@ -3,10 +3,15 @@
  */
 
 
-// alert("hello");
+alert("hello");
 document.write("hello I am from document.write")
 
 // inner html 
 document.getElementById("innerTest").innerHTML='<input type="text" name="js" id="js">';
 
-//documen
+//console
+console.log("hello from console");
+
+//print the current page
+
+// window.print()
